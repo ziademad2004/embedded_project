7cce6df8b37e2735655c5447d51464b17ed65d78
ziademad2004 <ziade8616@gmail.com>
2025-04-26 17:55:45 +0300

    first draft for tic tac toe

---
+++ tic_tac_toe_frist_draft/main.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "tic_tac_toe_frist_draft_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    MainWindow w;
+    w.setWindowTitle("tic tac toe");
+    w.setGeometry(0,0,600,800);
+    w.show();
+    return a.exec();
+}
